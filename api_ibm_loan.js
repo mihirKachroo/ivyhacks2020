d970507762287e8bba089bd18ea9192d4e79d77d
John Wright Stanly <jwstanly@audiodub.app>
2020-10-03 11:53:38 -0400

    new auth procedure for IBM

--- api_ibm_loan.js
+++ api_ibm_loan.js
@@ -1,32 +1,8 @@
-const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const btoa = require("btoa");
-var assert = require('assert');
-const iam_credentials = new Map();
+var request = require( 'request' );
+var btoa    = require( "btoa" );
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-// ----- (MODIFIED) IBM CODE ----- 
-
-// NOTE: you must manually construct iam_credentials hash map below using information retrieved
-// from your IBM Cloud.
-
-let rawCredData = fs.readFileSync('./ibm_iam.json');
-let credData = JSON.parse(rawdata);
-
-iam_credentials.set("url", credData["url"]);
-iam_credentials.set("username", credData["username"]);
-iam_credentials.set("password", credData["password"]);
-
-function apiGet(url, username, password, loadCallback, errorCallback){
-	const oReq = new XMLHttpRequest();
-	const tokenHeader = "Basic " + btoa((username + ":" + password));
-	const tokenUrl = url + "/identity/token";
-
-	oReq.addEventListener("load", loadCallback);
-	oReq.addEventListener("error", errorCallback);
-	oReq.open("GET", tokenUrl);
-	oReq.setRequestHeader("Authorization", tokenHeader);
-	oReq.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-	oReq.send();
-}
+let debugging = true;
 
 function apiPost(scoring_url, token, payload, loadCallback, errorCallback){
 	const oReq = new XMLHttpRequest();
@@ -39,97 +15,58 @@ function apiPost(scoring_url, token, payload, loadCallback, errorCallback){
 	oReq.send(payload);
 }
 
-// ----- JWSTANLY CODE ----- 
-
-const fields = ["Loan_ID","Gender","Married","Dependents","Education","Self_Employed","ApplicantIncome","CoapplicantIncome","LoanAmount","Loan_Amount_Term","Credit_History","Property_Area","Loan_Status"];
-
 function get_loan_eligibility(values){
-    assert(Array.isArray(values));
-    
-    apiGet(iam_credentials.get("url"),
-        iam_credentials.get("username"),
-        iam_credentials.get("password"),
-        function (res) {
-            let parsedGetResponse;
+    //values - list of model input values
+    //values example - ["LP001002","Male","No","0","Graduate","No",5849,0,128,360,1,"Urban"]
+
+    var apikey = "CdbrjiqNa6oNEcda7YKTgKUEKSlBgV_LueM16cZs6iUh";
+    var IBM_Cloud_IAM_uid = "bx";
+    var IBM_Cloud_IAM_pwd = "bx";
+
+    var iam_options = { url     : "https://iam.bluemix.net/oidc/token",
+                    headers : { "Content-Type"  : "application/x-www-form-urlencoded",
+                                "Authorization" : "Basic " + btoa( IBM_Cloud_IAM_uid + ":" + IBM_Cloud_IAM_pwd ) },
+                    body    : "apikey=" + apikey + "&grant_type=urn:ibm:params:oauth:grant-type:apikey" };
+                    
+    const fields = ["Loan_ID","Gender","Married","Dependents","Education","Self_Employed","ApplicantIncome","CoapplicantIncome","LoanAmount","Loan_Amount_Term","Credit_History","Property_Area","Loan_Status"];
+
+    request.post( iam_options, function( error, response, body ){
+        
+        if(debugging){
+            console.log("Response:\n"+JSON.stringify(response));
+            console.log("Body:\n" + JSON.stringify(body));
+        } 
+
+        var iam_token = JSON.parse( body )["access_token"];
+        const payload = '{"fields": ' + 
+                        '["' + fields.join('","') + '"],' +
+                        '"values": '+
+                        '[["' + values.join('","') + '"]]}';
+        const scoring_url = "https://us-south.ml.cloud.ibm.com/ml/v4/deployments/0f25fe4d-cc51-4b86-860a-876184d5e49e/predictions";
+        
+        if(debugging) console.log("IAM Token:\n" + JSON.stringify(iam_token));
+
+        apiPost(scoring_url, iam_token, payload, function(resp){
+            let parsedPostResponse;
             try {
-                parsedGetResponse = JSON.parse(this.responseText);
-            } catch(ex) {
-                console.log("JSON parse failed: error report below");
+                if(debugging){
+                    console.log("Resp type: " + typeof resp);
+                    console.log("Resp:\n"+resp);
+                    console.log("This:\n:"+ JSON.stringify(this));
+                }
+                parsedPostResponse = JSON.parse(this.responseText);
+            } catch (ex) {
+                console.log("Post callback parse failed")
                 console.error(ex);
             }
-            if (parsedGetResponse && parsedGetResponse.token) {
-                const token = parsedGetResponse.token
-                const iamToken = "Bearer " + token;
+            console.log("Scoring response");
+            console.log(parsedPostResponse);
+        }, function(){
+            console.error("Post callback failed")
+        });
+    } );
 
-                // NOTE: manually define and pass the array(s) of values to be scored in the next line
-                const payload = '{"fields": ' + 
-                                '["' + fields.join('","') + '"],' +
-                                '"values": '+
-                                '[["' + values.join('","') + '"]]}';
-                const scoring_url = "https://us-south.ml.cloud.ibm.com/ml/v4/deployments/4cf2d6c4-e80f-4376-8df8-070708bdd21d/predictions";
-
-                apiPost(scoring_url, iamToken, payload, function (resp) {
-                    let parsedPostResponse;
-                    try {
-                        parsedPostResponse = JSON.parse(this.responseText);
-                    } catch (ex) {
-                        // TODO: handle parsing exception
-                    }
-                    console.log("Scoring response");
-                    console.log(parsedPostResponse);
-                    return parsedPostResponse;
-                }, function (error) {
-                    console.log("Post failed: error report below");
-                    console.error(error);
-                });
-            } else {
-                console.error("Failed to retrieve Bearer token");
-            }
-        }, function (err) {
-            console.log("Callback failed: error report below");
-            console.error(err);
-        }
-    );
 }
 
-//sample results
-/*{
-    "predictions": [
-        {
-            "fields": [
-                "Loan_ID",
-                "Gender",
-                "Married",
-                "Dependents",
-                "Education",
-                "Self_Employed",
-                "ApplicantIncome",
-                "CoapplicantIncome",
-                "LoanAmount",
-                "Loan_Amount_Term",
-                "Credit_History",
-                "Property_Area",
-                "Loan_Status",
-                "Partition"
-            ],
-            "values": [
-                [
-                    "LP002995",
-                    "Male",
-                    "Yes",
-                    "2",
-                    "Graduate",
-                    "No",
-                    75834,
-                    0,
-                    187,
-                    360,
-                    1,
-                    "Urban",
-                    "d",
-                    "1_Training"
-                ]
-            ]
-        }
-    ]
-}//*/
+const vals = ["LP001002","Male","No","0","Graduate","No",5849,0,128,360,1,"Urban"];
+get_loan_eligibility(vals);
