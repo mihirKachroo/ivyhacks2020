815647fda7c3e318bd98e037753bbd02edb87312
John Wright Stanly <jwstanly@audiodub.app>
2020-10-03 15:58:44 -0400

    support for viewing existing campaigns

--- Frontend/src/pages/campaigns/Typography.js
+++ Frontend/src/pages/campaigns/Typography.js
@@ -7,6 +7,8 @@ import {
   FormHelperText,
   TextField,
   MenuItem,
+  Card,
+  CardMedia,
 } from "@material-ui/core";
 
 // styles
@@ -138,25 +140,54 @@ export default function TypographyPage() {
     <>
       <PageTitle title="My Campaigns" />
       <Grid container spacing={4}>
+        {/*each grid for a running campaign*/}
         <Grid item xs={12} md={6}>
-          <Widget title="Current Campaign" disableWidgetMenu>
+          <Widget title="Campaign Title Goes Here" disableWidgetMenu>
             <div className={classes.dashedBorder}>
-              <Typography variant="h1" className={classes.text}>
-                h1. Heading
-              </Typography>
-              <Typography variant="h2" className={classes.text}>
-                h2. Heading
-              </Typography>
-              <Typography variant="h3" className={classes.text}>
-                h3. Heading
-              </Typography>
+              <Card className={classes.root}>
+                <CardMedia
+                  className={classes.media}
+                  image="../../images/test.jpg"
+                  title="I need a new car!"
+                />
+              </Card>
               <Typography variant="h4" className={classes.text}>
-                h4. Heading
+                Amount raised: $[current amount pledged]/[amount needed]
               </Typography>
-              <Typography variant="h5" className={classes.text}>
-                h5. Heading
+              <Typography variant="h6" className={classes.text}>
+                Funds Needed By: [date here]
               </Typography>
-              <Typography variant="h6">h6. Heading</Typography>
+              <Typography variant="h6" className={classes.text}>
+                Repayment By: [date here]
+              </Typography>
+              <Typography variant="h8" className={classes.text}>
+                Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here.Description goes here.Description goes here.Description goes here. Description goes here. Description goes here. 
+              </Typography>
+            </div>
+          </Widget>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Widget title="Campaign Title Goes Here" disableWidgetMenu>
+            <div className={classes.dashedBorder}>
+              <Card className={classes.root}>
+                <CardMedia
+                  className={classes.media}
+                  image="../../images/test.jpg"
+                  title="I need a new car!"
+                />
+              </Card>
+              <Typography variant="h4" className={classes.text}>
+                Amount raised: $[current amount pledged]/[amount needed]
+              </Typography>
+              <Typography variant="h6" className={classes.text}>
+                Funds Needed By: [date here]
+              </Typography>
+              <Typography variant="h6" className={classes.text}>
+                Repayment By: [date here]
+              </Typography>
+              <Typography variant="h8" className={classes.text}>
+                Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here. Description goes here.Description goes here.Description goes here.Description goes here. Description goes here. Description goes here. 
+              </Typography>
             </div>
           </Widget>
         </Grid>
@@ -249,55 +280,6 @@ export default function TypographyPage() {
                 </TextField>
                 <br></br>
               </form>
-            </div>
-          </Widget>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Widget title="Basic Text Settings" disableWidgetMenu>
-            <div className={classes.dashedBorder}>
-              <Typography className={classes.text}>Basic text</Typography>
-              <Typography className={classes.text} weight="light">
-                Basic light text
-              </Typography>
-              <Typography className={classes.text} weight="medium">
-                Basic medium text
-              </Typography>
-              <Typography className={classes.text} weight="bold">
-                Basic bold text
-              </Typography>
-              <Typography className={classes.text}>
-                BASIC UPPERCASE TEXT
-              </Typography>
-              <Typography className={classes.text}>
-                basic lowercase text
-              </Typography>
-              <Typography className={classes.text}>
-                Basic Capitalized Text
-              </Typography>
-              <Typography>
-                <i>Basic Cursive Text</i>
-              </Typography>
-            </div>
-          </Widget>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Widget title="Text Size" disableWidgetMenu>
-            <div className={classes.dashedBorder}>
-              <Typography className={classes.text} size="sm">
-                Heading Typography SM Font Size
-              </Typography>
-              <Typography className={classes.text}>
-                Heading Typography Regular Font Size
-              </Typography>
-              <Typography className={classes.text} size="md">
-                Heading Typography MD Font Size
-              </Typography>
-              <Typography className={classes.text} size="xl">
-                Heading Typography XL Font Size
-              </Typography>
-              <Typography className={classes.text} size="xxl">
-                Heading Typography XXL Font Size
-              </Typography>
             </div>
           </Widget>
         </Grid>
