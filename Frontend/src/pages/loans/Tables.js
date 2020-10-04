1ffd51be5dcc6dd1e770b2cf7203b1a679c10e9a
John Wright Stanly <jwstanly@audiodub.app>
2020-10-04 10:59:01 -0400

    specified info on front end for IBM loan eligibility score

--- Frontend/src/pages/loans/Tables.js
+++ Frontend/src/pages/loans/Tables.js
@@ -1,28 +1,28 @@
 import React from "react";
-import { Grid, TextField, Button } from "@material-ui/core";
+import { Grid, TextField, Button, Typography } from "@material-ui/core";
 import MUIDataTable from "mui-datatables";
 
 // components
 import PageTitle from "../../components/PageTitle/PageTitle";
 
 const datatableData = [
-  ["Help me pay rent!", "John Doe", "Yonkers, NY", "$215/830"],
-  ["Need to pay medical bills", "Bill Smith", "Hartford, CT", "$795/1009"],
-  ["House repairs", "Cole Yaffee", "Tampa, FL", "$300/650"],
-  ["Car rent", "Sameh Arsalla", "Dallas, TX", "$40/230"],
-  ["Assisstance with child care", "Gavin Hazlett", "Hartford, CT", "$120/160"],
-  ["I need help with insulin costs", "Gavin Dickenson", "Yonkers, NY", "$300/480"],
-  ["Covid-19 relief for Mom", "Rajan Patel", "Hartford, CT", "$600/760"],
-  ["Help me buy a gift for my son's bithday", "Ethan Mere", "Tampa, FL", "$25/30"],
-  ["House rent help please!", "Maxwell Taylor", "Hartford, CT", "$500/550"],
-  ["Mobile home purchase help", "Tommas Hardee", "Yonkers, NY", "$1250/8000"],
-  ["Medicial bills from heart attack too much", "Max Tenhulzen", "Dallas, TX", "$800/1158"],
-  ["Lost my Job... need financial help", "Anton Roche", "Yonkers, NY", "$10/2500"],
-  ["Help with my two daughters", "Sai Sivakumar", "Dallas, TX", "$30/350"],
-  ["My family needs to move for a new job", "Jack Brown", "Yonkers, NY", "$1200/1250"],
-  ["Home flooded from Hurricane", "Kevin Nguyen", "Hartford, CT", "$70/1800"],
-  ["Family member incarcerated; need help", "Ronak Venkata", "Tampa, FL", "$255/300"],
-  ["Help me suprise my wife for our anniversary", "David Huang", "Tampa, FL", "$35/50"],
+  ["Help me pay rent!", "John Doe", "Yonkers, NY", "Yes (43%)", "$215/830"],
+  ["Need to pay medical bills", "Bill Smith", "Hartford, CT", "Yes (78%)", "$795/1009"],
+  ["House repairs", "Cole Yaffee", "Tampa, FL", "Yes (21%)","$300/650"],
+  ["Car rent", "Sameh Arsalla", "Dallas, TX", "No (53%)", "$40/230"],
+  ["Assisstance with child care", "Gavin Hazlett", "Hartford, CT", "Yes (91%)","$120/160"],
+  ["I need help with insulin costs", "Gavin Dickenson", "Yonkers, NY", "Yes (74%)", "$300/480"],
+  ["Covid-19 relief for Mom", "Rajan Patel", "Hartford, CT","Yes (68%)", "$600/760"],
+  ["Help me buy a gift for my son's bithday", "Ethan Mere", "Tampa, FL", "Yes (98%)","$25/30"],
+  ["House rent help please!", "Maxwell Taylor", "Hartford, CT", "No (9%)", "$500/550"],
+  ["Mobile home purchase help", "Tommas Hardee", "Yonkers, NY", "Yes (38%)", "$1250/8000"],
+  ["Medicial bills from heart attack too much", "Max Tenhulzen", "Dallas, TX", "Yes (78%)", "$800/1158"],
+  ["Lost my Job... need money", "Anton Roche", "Yonkers, NY", "No (96%)", "$10/2500"],
+  ["Help with my two daughters", "Sai Sivakumar", "Dallas, TX", "Yes (58%)", "$30/350"],
+  ["My family needs to move for a new job", "Jack Brown", "Yonkers, NY", "Yes (69%)", "$1200/1250"],
+  ["Home flooded from Hurricane", "Kevin Nguyen", "Hartford, CT", "No (39%)", "$70/1800"],
+  ["Family member incarcerated; need help", "Ronak Venkata", "Tampa, FL", "Yes (67%)", "$255/300"],
+  ["Help me suprise my wife for our anniversary", "David Huang", "Tampa, FL", "Yes (90%)", "$35/50"],
 ];
 
 export default function Tables() {
@@ -42,11 +42,16 @@ export default function Tables() {
           <MUIDataTable
             title="Creditors List"
             data={datatableData}
-            columns={["Campaign Title", "Name", "Location", "Amount Raised"]}
+            columns={["Campaign Title", "Name", "Location", "IBM Loan Eligibility Score*", "Amount Raised"]}
             options={{
               filterType: "checkbox",
             }}
           />
+        </Grid>
+      </Grid>
+      <Grid container spacing={4}>
+        <Grid item xs={3.5}>
+            * IBM Loan Eligibility Score: reports if the candidate's history shows they should repay the loan (Yes) or not (No), along with a confidence score based on the report from 0-100% 
         </Grid>
       </Grid>
     </>
